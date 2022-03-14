da375f81ce792d1ff577e58fb367ce20355cc4c1
Julia Hartnett <62521409+juliahartnett@users.noreply.github.com>
2022-03-14 11:47:38 -0400

    Added eclipse project

---
+++ hotelSystem/src/hotelSystem/validation.java
@@ -0,0 +1,85 @@
+package hotelSystem;
+import java.util.Scanner;
+
+public class validation {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static String validatePasswords(String password){
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+        // validate length of password is >= 8 characters
+        char passShort = 'n';       
+        if(password.length() >= 8) {
+            passShort = 'y';    
+        }
+        while(passShort != 'y') {
+            System.out.println("Password not long enough! Password must contain 8 characters.\nEnter new password: ");
+            password = scan.next();
+            if(password.length() > 8) {
+                passShort = 'y';    
+            }
+
+        //validate if special characters are included
+        char passSpecial = 'n';
+        if(password.contains("@") | password.contains("#") | password.contains("!") | password.contains("$") | password.contains("&") | password.contains("*")){
+            passSpecial= 'y';
+        }
+        while(passSpecial != 'y'){
+            System.out.println("This is not a valid password! Password must contain at least one of the following special characters: !, #, $, @, &, or *.\nEnter new password: ");
+            password = scan.next();
+            if(password.contains("@") | password.contains("#") | password.contains("!") | password.contains("$") | password.contains("&") | password.contains("*")){
+                passSpecial = 'y';
+            }
+        }
+
+        //validate length of password <= 50 characters
+        char passLong = 'n';
+        if(password.length() <= 50){
+            passLong = 'y';
+        }
+        while(passLong != 'y'){
+            System.out.println("This password is too long! Password must contain 50 characters or less.\nEnter new password: ");
+            password = scan.next();
+            if(password.length() <= 50){
+                passLong = 'y';
+            }
+        }
+
+        //validate if password contains digit
+        char passNum = 'n';
+        if (password.contains("0") | password.contains("1") | password.contains("2") | password.contains("3") | password.contains("4") | password.contains("5") | password.contains("6") | password.contains("7") | password.contains("8") | password.contains("9")){
+            passNum = 'y';
+        }
+        while (passNum != 'y'){
+            System.out.println("This password does not contain any digits. Your password must contain at least one digit.\nEnter new password: ");
+            password = scan.next();
+            if(password.contains("0") | password.contains("1") | password.contains("2") | password.contains("3") | password.contains("4") | password.contains("5") | password.contains("6") | password.contains("7") | password.contains("8") | password.contains("9")){
+                passNum = 'y';
+            }
+        }
+
+        //validate if password contains at least one capital letter
+        char passCap = 'n';
+        for (int i=0; i < password.length(); i++){
+            if(Character.isUpperCase(password.charAt(i))){
+                passCap = 'y';
+            }
+            while (passCap != 'y'){
+                System.out.println("This password does not contain a capital letter. Your password must contain at least one capital letter.\nEnter new password: ");
+                password = scan.next();
+                if(Character.isUpperCase(password.charAt(i))){
+                    passCap = 'y';
+                }
+            }
+        }
+    } // end validation loop     
+        return password;
+    }
+
+}
+
---
+++ hotelSystem/src/module-info.java
@@ -0,0 +1,2 @@
+module hotelSystem {
+}
