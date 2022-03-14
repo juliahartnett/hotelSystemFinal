84306435886d394d9060d56cecc5e35edc2f841f
Helen He <79013599+HelenHe1028@users.noreply.github.com>
2022-03-14 16:18:23 -0400

    Add files via upload

---
+++ hotelSystem/src/hotelSystem/emailValidation.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class emailValidation {
+    public static void main(String[] args){
+            
+    } 
+    public static String validateEmail(String email){
+
+        Scanner scan = new Scanner(System.in);
+        //validate the format of the email users entered
+        String reg = "^(\\w+([-.][A-Za-z0-9]+)*){3,18}@\\w+([-.][A-Za-z0-9]+)*\\.\\w+([-.][A-Za-z0-9|com|org|net|edu|gov|co.uk]+)*$";
+        char emailFormat = 'n';
+        while(emailFormat !='y') {
+            System.out.println("Your Email address must contain '@'' and '.' ");
+            email = scan.next();
+            if (email.matches(reg)){
+                emailFormat = 'y';
+            }
+            //To the left of "@":
+            //Letters, numbers from 0 to 9, underscores
+            //minus sign, dot
+            //The length is 3 to 18 characters
+            //Cannot end with a dot, minus sign or underscore
+
+            //To the Right of "@":
+            //Letters, numbers from 0 to 9, underscores
+            //There can only be one dot, and there cannot be an empty space between the dot and "@"
+            //minus sign, dot
+            //Cannot end with a dot, minus sign or underscore
+        }
+    //char emailDomain = 'n';
+    //while(emailDomain !='y'){
+      //  System.out.println("Your email domain is no valied");
+        //email = scan.next();
+        //if(email.matches){
+
+        //}
+    //}
+
+    return email;
+    }
+}
