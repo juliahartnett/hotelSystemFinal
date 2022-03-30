e0f4619d18b75aee781ed0129ee8d7dbfea8f1d4
Julia Hartnett <62521409+juliahartnett@users.noreply.github.com>
2022-03-30 11:28:49 -0400

    Add files via upload

--- hotelSystem/src/hotelSystem/emailValidation.java
+++ hotelSystem/src/hotelSystem/emailValidation.java
@@ -12,12 +12,18 @@ public class emailValidation {
         String reg = "^(\\w+([-.][A-Za-z0-9]+)*){3,18}@\\w+([-.][A-Za-z0-9]+)*\\.\\w+([-.][com|org|net|edu|gov|co.uk])*$";
         //System.out.println("Enter your email address: ");
 		//String s = scan.next();
+        char emailGood = 'n';
+        while(emailGood == 'n') {
         if (email.matches(reg)) {
             //System.out.println("Congratulations, you have successfully registered.");
+        	emailGood = 'y';
             }
             else {
-            System.out.println("Your Email address must contain '@'' and '.', the length before @ is 3-18 characters and cannot end with a dot. The domain have to be 'com, org, net, edu, gov and co.uk, no empty between dot and '@''");
+            	System.out.println("Your Email address must contain '@' and '.', the length before te @ is 3-18 characters and the email cannot end with a dot. The domain has to be '.com', '.org', '.net', '.edu', '.gov' or '.co.uk', and there can be no empty between '.' and '@'");
+            	System.out.println("Enter a new email address: ");
+            	email = scan.next();
             }
+        }
     return email;
     }
 }
