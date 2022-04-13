dbed49d7958061d204bc177015aa9cd2db11c97d
hannahalshech <79874128+hannahalshech@users.noreply.github.com>
2022-04-13 11:45:11 -0400

    Add files via upload

--- hotelSystem/src/hotelSystem/validation.java
+++ hotelSystem/src/hotelSystem/validation.java
@@ -4,39 +4,17 @@ import java.util.Scanner;
 public class validation {
 
 	public static void main(String[] args) {
-	
 		
 	}
 
-    public static String validatePhone (String phoneNumber, Scanner scan){
-        //validate phone number
-        String phoneReg = "(0/91)?[7-9][0-9]{9}";
-        char phoneGood = 'n';
-        if (phoneNumber.matches(phoneReg)){
-            phoneGood = 'y';
-        }
-        while (phoneGood != 'y'){
-            System.out.println("Your phone number is invalid. Remember to include your 3-digit area code, along with your 7-digit phone number. Please try again: ");
-            phoneNumber = scan.next();
-            if (phoneNumber.matches(phoneReg)){
-                phoneGood = 'y';
-            }
-        }
-        //end validation loop
-        return phoneNumber;
-    }
-	
-	public static String validateCheckin (String checkin, Scanner scan){
+    public static String validateCheckin (String checkin){
 
-
+        Scanner scan = new Scanner(System.in);
         //validate check in time
 		String checkinReg = "([01]?[0-9]|2[0-3]):[0-5][0-9]";
         char checkinTime = 'n';
-        if (checkin.matches(checkinReg)){
-            checkinTime = 'y';
-        }
         while(checkinTime != 'y') {
-            System.out.println("Your check in time is invalid. Remember, your check-in time must be in the military (24 hour) format. Please try again: ");
+            System.out.println("Your check in time is invalid. Please try again: ");
             checkin = scan.next();
             if (checkin.matches(checkinReg)){
                 checkinTime = 'y';
@@ -46,16 +24,13 @@ public class validation {
         return checkin;
     }
     
-    public static String validateCheckout (String checkout, Scanner scan){
- 
+    public static String validateCheckout (String checkout){
+        Scanner scan = new Scanner(System.in);
         //validate check out time
         String checkoutReg = "([01]?[0-9]|2[0-3]):[0-5][0-9]";
         char checkoutTime = 'n';
-        if (checkout.matches(checkoutReg)){
-            checkoutTime = 'y';
-        }
         while(checkoutTime != 'y') {
-            System.out.println("Your check out time is invalid. Remember, your check-out time must be in the military (24 hour) format. Please try again: ");
+            System.out.println("Your check out time is invalid. Please try again: ");
             checkout = scan.next();
             if (checkout.matches(checkoutReg)){
                 checkoutTime = 'y';
@@ -66,7 +41,9 @@ public class validation {
     return checkout;
 }
 	
-	public static String validatePasswords(String password, Scanner scan){
+	public static String validatePasswords(String password){
+		
+		Scanner scan = new Scanner(System.in);
 		
 		
         // validate length of password is >= 8 characters
