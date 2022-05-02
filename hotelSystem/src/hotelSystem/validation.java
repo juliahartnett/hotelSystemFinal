081ef938dde63c8e3c99d36e08ce1e837169a096
hannahalshech <79874128+hannahalshech@users.noreply.github.com>
2022-05-02 11:36:31 -0400

    Update validation.java

--- hotelSystem/src/hotelSystem/validation.java
+++ hotelSystem/src/hotelSystem/validation.java
@@ -64,74 +64,20 @@ public class validation {
 }
 	
 	public static String validatePasswords(String password, Scanner scan){
-		
-		
-        // validate length of password is >= 8 characters
-        char passShort = 'n';       
-        if(password.length() >= 8) {
-            passShort = 'y';    
+        //validate passwords
+        String passwordReg = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$";
+        char passwordTime = 'n';
+        if (password.matches(passwordReg)){
+            passwordTime = 'y';
         }
-        while(passShort != 'y') {
-            System.out.println("Password not long enough! Password must contain 8 characters.\nEnter new password: ");
+        while(passwordTime != 'y'){
+            System.out.println("Your password is invalid. Remember, your password must be at least 8 characters but no more than 20, and contain at least one uppercase letter, one number, and one special character.");
             password = scan.next();
-            if(password.length() > 8) {
-                passShort = 'y';    
-            }
-
-        //validate if special characters are included
-        char passSpecial = 'n';
-        if(password.contains("@") | password.contains("#") | password.contains("!") | password.contains("$") | password.contains("&") | password.contains("*")){
-            passSpecial= 'y';
-        }
-        while(passSpecial != 'y'){
-            System.out.println("This is not a valid password! Password must contain at least one of the following special characters: !, #, $, @, &, or *.\nEnter new password: ");
-            password = scan.next();
-            if(password.contains("@") | password.contains("#") | password.contains("!") | password.contains("$") | password.contains("&") | password.contains("*")){
-                passSpecial = 'y';
+            if (password.matches(passwordReg)){
+                passwordTime = 'y';
             }
         }
-
-        //validate length of password <= 50 characters
-        char passLong = 'n';
-        if(password.length() <= 50){
-            passLong = 'y';
-        }
-        while(passLong != 'y'){
-            System.out.println("This password is too long! Password must contain 50 characters or less.\nEnter new password: ");
-            password = scan.next();
-            if(password.length() <= 50){
-                passLong = 'y';
-            }
-        }
-
-        //validate if password contains digit
-        char passNum = 'n';
-        if (password.contains("0") | password.contains("1") | password.contains("2") | password.contains("3") | password.contains("4") | password.contains("5") | password.contains("6") | password.contains("7") | password.contains("8") | password.contains("9")){
-            passNum = 'y';
-        }
-        while (passNum != 'y'){
-            System.out.println("This password does not contain any digits. Your password must contain at least one digit.\nEnter new password: ");
-            password = scan.next();
-            if(password.contains("0") | password.contains("1") | password.contains("2") | password.contains("3") | password.contains("4") | password.contains("5") | password.contains("6") | password.contains("7") | password.contains("8") | password.contains("9")){
-                passNum = 'y';
-            }
-        }
-
-        //validate if password contains at least one capital letter
-        char passCap = 'n';
-        for (int i=0; i < password.length(); i++){
-            if(Character.isUpperCase(password.charAt(i))){
-                passCap = 'y';
-            }
-            while (passCap != 'y'){
-                System.out.println("This password does not contain a capital letter. Your password must contain at least one capital letter.\nEnter new password: ");
-                password = scan.next();
-                if(Character.isUpperCase(password.charAt(i))){
-                    passCap = 'y';
-                }
-            }
-        }
-    } // end validation loop     
+    // end validation loop     
         return password;
     }
 
